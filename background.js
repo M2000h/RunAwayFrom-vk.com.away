337037f94193a87e3814330b9b3e3ed81f199d26
Maxim Shakura <43370239+M2000h@users.noreply.github.com>
2019-05-30 11:48:58 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onCreated.addListener( function (tabId, changeInfo, tab) {
 	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
 		var url = tabs[0].url;
-		if (url.indexOf("vk.com/away.php", 0)>=0){
+		if (url.substr(0,23) == "https://vk.com/away.php"){
 			var last = url.indexOf("&", 0)
 			if(last == -1) last = 1000;
 			var url = decodeURIComponent(url.substr(27, last-27));
